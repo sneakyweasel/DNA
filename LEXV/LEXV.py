8533b8b69954f7567abba4045fc5776daa4c3cd8
Philippe Cochin <philippe.cochin@gmail.com>
2018-06-04 11:44:39 +0200

    Solved LEXV

--- LEXV/LEXV.py
+++ LEXV/LEXV.py
@@ -6,14 +6,25 @@ import itertools
 file = open(os.path.join(os.path.dirname(sys.argv[0]), 'rosalind_lexv.txt'))
 lines = file.readlines()
 alphabet = lines[0].strip("\n").split(" ")
-alphabet.append(" ")
 num = int(lines[1])
-# print(alphabet)
+length = len(alphabet)
+# print(f"ALP: {alphabet} - LEN: {length}")
 # print(num)
+# print("-----")
 
 arr = []
 for x in range(1, num+1):
-    strings = (list(itertools.product(alphabet, repeat = x)))
-    for string in strings:
-        arr.append("".join(string))
-print(arr)
+    arr += list(itertools.product(range(len(alphabet)), repeat = x))
+sorts = sorted(arr)
+# print(sorts)
+
+results = []
+for numstr in sorts:
+    strand = ""
+    for c in numstr:
+        strand += alphabet[c]
+    results.append(strand.ljust(num, ' '))
+# print(results)
+
+for result in results:
+    print(result)
