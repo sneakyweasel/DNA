fc463431934515af093642d63ef9e78e33e38755
Philippe Cochin <philippe.cochin@gmail.com>
2018-06-21 00:15:39 +0200

    Finally GRAPH

--- GRPH/GRPH.py
+++ GRPH/GRPH.py
@@ -2,73 +2,52 @@ import os
 import sys
 import string
 import itertools
-from graphviz import Digraph
 from Bio import SeqIO
 from Bio.Alphabet import IUPAC
+import networkx as nx
+import matplotlib.pyplot as plt
+import pygraphviz as pgv
+
 
 # Read input file to seqs
 file = open(os.path.join(os.path.dirname(sys.argv[0]), 'rosalind_grph.txt'))
 records = list(SeqIO.parse(file, "fasta"))
 k = 3
-
 print(len(records))
 
-# Longest common subsequence
-def lcs(a, b):
-    lengths = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
-    # row 0 and column 0 are initialized to 0 already
-    for i, x in enumerate(a):
-        for j, y in enumerate(b):
-            if x == y:
-                lengths[i+1][j+1] = lengths[i][j] + 1
-            else:
-                lengths[i+1][j+1] = max(lengths[i+1][j], lengths[i][j+1])
-    # read the substring out from the matrix
-    result = ""
-    x, y = len(a), len(b)
-    while x != 0 and y != 0:
-        if lengths[x][y] == lengths[x-1][y]:
-            x -= 1
-        elif lengths[x][y] == lengths[x][y-1]:
-            y -= 1
-        else:
-            assert a[x-1] == b[y-1]
-            result = a[x-1] + result
-            x -= 1
-            y -= 1
-    return result
-
 # Suffix and prefix lcs
-def fix(a, b):
+def fix(a, b, k):
     result = []
-    for x in range(0, len(a)):
-        suffix = a[-x:]
-        prefix = b[:x]
-        if prefix == suffix:
-            result.append(prefix)
-            # print(f"X: {x} - PreA: {prefix} | SufB: {suffix}")
-    if len(sorted(result)) > 0:
-        return sorted(result)[-1]
+    suffix_a = a[-k:]
+    prefix_a = a[:k]
+    suffix_b = b[-k:]
+    prefix_b = b[:k]
+    # if prefix_a == suffix_b:
+    #     result.append(str(prefix_a))
+    if prefix_b == suffix_a:
+        return(str(prefix_b))
     else:
         return ""
 
+# A length k suffix of s that matches a length k prefix of t, as long as s≠t
+# suffix = s[:k]
+# prefix = t[:k]
+
 # Create directed graph
-f = Digraph('finite_state_machine', filename='fsm.gv')
-f.attr(rankdir='LR', size='8,5')
-f.attr('node', shape='doublecircle')
+G = nx.DiGraph()
 
 for pair in itertools.product(records, repeat=2):
-    # print(f"{pair[0].id} {pair[1].id}")
-    sub = fix(pair[0].seq, pair[1].seq)
-    if len(sub) == 3 and (pair[0].seq != pair[1].seq):
-        f.edge(pair[0].id, pair[1].id, label=str(sub))
-        print(f"{pair[0].id} {pair[1].id}")
-f.view()
+    s = str(pair[0].seq)
+    t = str(pair[1].seq)
+    sub = fix(s, t, 3)
 
+    if len(sub) == 3 and s != t:
+        G.add_edge(pair[0].id, pair[1].id, str=str(sub))
 
+for line in nx.generate_edgelist(G, data=False):
+    print(line)
 
-# AAATAAA           (0498)
-#     AAATCCC       (0442)
-#     AAATTTT       (2391)
-#        TTTTCCC    (2323)
-# GGGTGGG           (5013)
+# Draw graph
+# AG = nx.nx_agraph.to_agraph(G)
+# AG.layout()
+# AG.draw(f'graph.png')
--- LIA/LIA.py
+++ LIA/LIA.py
@@ -8,8 +8,6 @@ file = open(os.path.join(os.path.dirname(sys.argv[0]), 'rosalind_lia.txt'))
 vars = file.read()
 k = int(vars.split(' ')[0]) # number of generations
 N = int(vars.split(' ')[1]) # number of Aa Bb organisms
-k = 2
-N = 1
 
 # [Aa Bb] + [Aa Bb]
 # AA (1) - homo dom
@@ -29,3 +27,14 @@ N = 1
 # Prob(Aa Bb) = Prob(Aa) * Prob(Bb)
 # Prob(Aa Bb) = 2/4 * 2/4
 # Total: 2
+
+def firstLaw():
+    total = AA + Aa + aa
+    totalComb = comb(total, 2)
+    dominant = comb(AA, 2)              \
+                + AA * Aa               \
+                + AA * aa               \
+                + .5 * Aa * aa          \
+                + .75 * comb(Aa, 2)
+    prob_dom = dominant / totalComb
+    return prob_dom
